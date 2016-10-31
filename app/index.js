3f6832faa7c3511cb856deefbcfc40d207a33f9a
TsengMingYu <qazwsx9006@hotmail.com>
2016-10-31 16:54:43 +0800

    practice life cycle

--- app/index.js
+++ app/index.js
@@ -1,43 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class MarkdownEditor extends React.Component {
+class MyComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.rawMarkup = this.rawMarkup.bind(this);
+    console.log('constructor');
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
-      value: 'Type some word.',
+      name: 'mingyu',
+      i: 0
     }
   }
 
-  handleChange(e) {
+  handleClick(e) {
     // e.target.value
-    this.setState({value: this.refs.textarea_ref.value});
+    this.setState({name: 'Ming Yu', i: this.state.i+1});
   }
 
-  rawMarkup(e) {
-    const md = new Remarkable();
-    return {__html: md.render(this.state.value)};
+  componentWillMount(e) {
+    console.log('will mount');
+    console.log(this.state);
   }
+
+  componentDidMount(e) {
+    console.log('did mount');
+    console.log(this.state);
+  }
+
+  componentWillReceiveProps(e) {
+    console.log('component will receive props');
+  }
+
+  componentWillUpdate(e) {
+    console.log('componentWillUpdate');
+    console.log(this.state);
+  }
+  componentDidUpdate(e) {
+    console.log('componentDidUpdate');
+    console.log(this.state);
+  }
+  componentWillUnmount(e) {
+    console.log('componentWillUnmount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(this.state);
+    console.log(nextState);
+    console.log('update check');
+    console.log(this.state);
+    return this.state.i > 4;
+    // return nextProps.id !== this.props.id;
+  }
+
+
 
   render() {
     return (
-      <div className="MarkdownEditor">
-        <h3>Input</h3>
-        <textarea
-          onChange={this.handleChange}
-          ref="textarea_ref"
-          defaultValue={this.state.value}
-          placeholder="Type here plz!"
-        >
-        </textarea>
-        <h3>Output</h3>
-        <div className="content" dangerouslySetInnerHTML={this.rawMarkup()}></div>
+      <div>
+        <h3 className='hell2' data-hello='123'>Input</h3>
+        <h4>{this.state.name}</h4>
+        <div onClick={this.handleClick}>Click</div>
       </div>
     )
   }
 
 }
 
-ReactDOM.render(<MarkdownEditor />, document.getElementById('app'));
+ReactDOM.render(<MyComponent />, document.getElementById('app'));
